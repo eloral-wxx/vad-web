90f0efdd32092c4f21355aaf6ea94cdf51d0f0d0
eloral-wxx <2330711901@qq.com>
2026-04-22 16:20:35 +0800

    修改route.ts的url，从子域名改回阿里云服务器公网ip+infer

--- app/api/infer/route.ts
+++ app/api/infer/route.ts
@@ -1,6 +1,6 @@
 export const runtime = "nodejs"
 
-const BACKEND_INFER_URL = "http://39.106.114.117:8000"
+const BACKEND_INFER_URL = "http://39.106.114.117:8000/infer"
 
 export async function POST(request: Request): Promise<Response> {
   try {
